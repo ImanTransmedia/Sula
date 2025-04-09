173c48b6e573eef43daa9d09749d13da6e45e8c6
Serghy Castillo <b1uefeet.dev@gmail.com>
2025-04-09 16:57:03 -0500

    feat(scripts): se agrega los scriptableObjects de prendas y regiones
    se agregaron los Scriptable Objects de las prendas y de las Regiones
    se define los colores de cada region

---
+++ Assets/_SULA/Scripts/Scriptable Object/Clothes.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Clothes", menuName = "Scriptable Objects/Clothes")]
+public class Clothes : ScriptableObject
+{
+    public string clotheName;
+    public double price;
+    public Sprite imagen;
+    public bool is3D;
+    public Color[] materialColors;
+    public GameObject prefab;
+    
+}
---
+++ Assets/_SULA/Scripts/Scriptable Object/Regions.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Regions", menuName = "Scriptable Objects/Regions")]
+public class Regions : ScriptableObject
+{
+    public string regionName;
+    public Color darkColor;
+    public Color lightColor;
+    public Color accentColor;
+
+    
+}
