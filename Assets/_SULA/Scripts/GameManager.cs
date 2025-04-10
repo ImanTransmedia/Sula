cf9df2930273702aac33822f8cfb864bef68606b
Serghy Castillo <b1uefeet.dev@gmail.com>
2025-04-10 09:31:23 -0500

    feat(pantalla): se agrega la pantalla de inicio

---
+++ Assets/_SULA/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton instance
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("GameManager");
+                    _instance = go.AddComponent<GameManager>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    [Header("Actual Region")]
+    public Regions ActualRegion { get; set; }
+    public Clothes ActualClothe { get; set; }
+
+    
+
+}
